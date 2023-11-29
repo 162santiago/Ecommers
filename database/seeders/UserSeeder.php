6f6307904a7ceae14f3ab3cd3c66cc7b6f7ae337
Santiago Matias <131896853+Santiago8888dev@users.noreply.github.com>
2023-11-29 18:59:29 -0500

    add migration user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+
+        ]);
+    }
+}
