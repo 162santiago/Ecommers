3f52eef693212846eaff38dc4b172a3804c87ff5
santiago <131896853+Santiago8888dev@users.noreply.github.com>
2023-12-18 18:22:14 -0500

    implemen password

--- app/Http/Controllers/Frontend/UserProfileController.php
+++ app/Http/Controllers/Frontend/UserProfileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
+use File;
 class UserProfileController extends Controller
 {
     public function index(){
@@ -16,23 +16,37 @@ class UserProfileController extends Controller
         $request->validate([
             'name' => ['required', 'max:50'],
             'email' => ['required', 'email', 'unique:users,email,'. Auth::user()->id],
-            'image' => ['nullable', 'max:2020', 'image']
+            'image' => ['max:2020', 'image']
         ]);
 
         $user = Auth::user();
         if ($request->hasFile('image')) {
-            if (File::exist(public_path($user->image))) {
+            if (File::exists(public_path($user->image))) {
                 File::delete(public_path($user->image));
             }
             $image = $request->image;
             $imageName = rand() .'_' . $image->getClientOriginalName();
-            $image->move(public_path('image/'.$imageName));
+            $image->move(public_path('image'),$imageName);
             $path = 'image/'.$imageName;
-            $request->image = $path;
+            $user->image = $path;
         }
-
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
         toastr()->success('profile update');
+        return redirect()->back();
+    }
 
-        // dd($request->all());
+    public function updatePassword(Request $request){
+        $request->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        $request->user()->update([
+            'password' => bcrypt($request->password)
+        ]);
+        toastr()->success('password update successfully');
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::group(['middleware' => ['auth', 'verified'],'prefix' => 'user', 'as' =>'u
     Route::get('/dashboard', [UserDashboardController::class,'index'])->name('dashboard');
     Route::get('/profile',[UserProfileController::class,'index'])->name('profile');
     Route::put('/profile', [UserProfileController::class, 'updateProfile'])->name('profile.update');
+    Route::post('/profile', [UserProfileController::class, 'updatePassword'])->name('profile.update.password');
 } );
 
 
